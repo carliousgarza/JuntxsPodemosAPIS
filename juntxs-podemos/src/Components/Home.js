0bf8f6e0e93034a6afb1adf67d28065f216a0bc5
Carlos <carlosfgarza@gmail.com>
2019-11-06 11:26:58 -0600

    warnings

--- juntxs-podemos/src/Components/Home.js
+++ juntxs-podemos/src/Components/Home.js
@@ -1,10 +1,6 @@
 import React, { Component } from "react"
 import "./Login.css"
 import firebase from '../config/firebase'
-import TextField from '@material-ui/core/TextField'
-import Button from '@material-ui/core/Button'
-import { Container } from "@material-ui/core"
-import Swal from "sweetalert2"
 import Navbar from './Navbar'
 
 class Home extends Component {
